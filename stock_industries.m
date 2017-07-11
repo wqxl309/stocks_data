6741945db1bb81c960460ec91be932f0fb804e47
wqxl309 <wqxl309@hotmail.com>
2017-07-11 13:28:55 +0800

    stk industry modified

--- stock_industries.m
+++ stock_industries.m
@@ -14,22 +14,16 @@ for dumi = 1:stknum
     stkcd = stkinfo{dumi,1};
     lasttrd = datestr(last_trddates{dumi},'yyyymmdd');
     swcode = cell2mat(w.wss(stkcd,'industry_swcode',['tradeDate=' lasttrd],'industryType=4'));
-    if isnan(swcode)
-        industry_codes(dumi,1) = str2double(stkcd(1:6));
-        industry_codes(dumi,2) = str2double(lasttrd);
+    industry_codes(dumi,1) = str2double(stkcd(1:6));
+    industry_codes(dumi,2) = str2double(lasttrd);
+    if isnan(swcode) 
         industry_codes(dumi,3:end)= NaN;
         continue;
     else
         swcodes = strsplit(swcode,'-');
-    end
-    if length(swcodes)<3
-        display([stkcd '''s swcodes is wrong'])
-    else
-        industry_codes(dumi,1) = str2double(stkcd(1:6));
-        industry_codes(dumi,2) = str2double(lasttrd);
-        industry_codes(dumi,3) = str2double(swcodes{1});
-        industry_codes(dumi,4) = str2double(swcodes{2});
-        industry_codes(dumi,5) = str2double(swcodes{3});
+        for dumk = 1:length(swcodes)
+            industry_codes(dumi,2+dumk) = str2double(swcodes{dumk});
+        end
         display([stkcd '''s swcodes updated'])
     end
 end
